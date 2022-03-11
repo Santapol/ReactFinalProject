cab19ac6162a15f0a272c641ea296bfd0ce1b191
Santapol <sa.santapol_st@tni.ac.th>
2022-03-11 21:29:17 +0700

    up6

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -19,12 +19,7 @@ function Navigation() {
                 <li className="nav-item">
                     <NavLink to="/resume" activeClassName="active-class" exact>Resume</NavLink>
                 </li>
-                <li className="nav-item">
-                    <NavLink to="/portfolios" activeClassName="active-class" exact>Portfolios</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink to="/blogs" activeClassName="active-class" exact>Blogs</NavLink>
-                </li>
+                
                 <li className="nav-item">
                     <NavLink to="/product" activeClassName="active-class" exact>Product</NavLink>
                 </li>
